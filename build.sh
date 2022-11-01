38e4164282ea05add6ae1ce10a03aff5bfcf3110
HUANG SIZHE <hsz1273327@gmail.com>
2022-11-01 17:32:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --platform=linux/arm64,linux/amd64 -t hsz1273327/redis-allinone:1.2.0 -t hsz1273327/redis-allinone:latest .
+docker buildx build --push --platform=linux/arm64,linux/amd64 -t hsz1273327/redis-allinone:redis6-1.2.1 -t hsz1273327/redis-allinone:redis6-latest .
